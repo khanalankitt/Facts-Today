89880cd52469e799564e6a61597f252770bde6fb
Ankit Khanal <akhanal749@gmail.com>
2024-03-09 22:10:46 +0545

    added login page css

--- factos/app/(components)/login/page.js
+++ factos/app/(components)/login/page.js
@@ -1,7 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Login() {
     return (
       <>
-        <h1>Login</h1>
+        <div className="login-container">
+          <h1>Login</h1>
+          <form>
+            <input type="email" placeholder="Email"/>
+            <input type="password" placeholder="Password" />
+            <input type="submit" className="submit" value="Login" />
+            <p>Don't have an account? <Link href="/signup">Signup</Link></p>
+            <hr />
+            <p className="por">or</p>
+          </form>
+          <button>
+            <Image
+              src="/google.png"
+              height={30}
+              width={30}
+            />
+            Login with Google
+          </button>
+        </div>
       </>
     );
   }
--- factos/app/layout.js
+++ factos/app/layout.js
@@ -13,6 +13,7 @@ export default function RootLayout({ children }) {
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
         <link href="https://fonts.googleapis.com/css2?family=Oxygen:wght@300;400;700&display=swap" rel="stylesheet"/>
+        <link rel="icon" type="image/x-icon" href="./favicon.ico"/>
       </head>
       <body className="oxygen-light">
         <div className="bodyContainer">
