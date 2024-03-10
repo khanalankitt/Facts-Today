f5f6f35d30726e24573cf896a22c2a2b5fa940c9
Ankit Khanal <akhanal749@gmail.com>
2024-03-10 20:35:51 +0545

    added github authentication

--- factos/app/(components)/login/page.js
+++ factos/app/(components)/login/page.js
@@ -1,29 +1,33 @@
 "use client"
 import Image from "next/image";
-import Link from "next/link";
 import { signIn } from "next-auth/react"
 export default function Login() {
     return (
       <>
         <div className="login-container loginsignup">
-          <h1>Login</h1>
-          <form>
-            <input type="email" placeholder="Email"/>
-            <input type="password" placeholder="Password" />
-            <input type="submit" className="submit" value="Login" />
-            <p>Don't have an account? <Link href="/signup">Signup</Link></p>
-            <hr />
-            <p className="por">or</p>
-          </form>
+          <h2>Login to get started</h2>
+          <hr />
           <button onClick={()=>{
                 signIn('google',{callbackUrl:"http://localhost/3000/"})
           }}>
+            <br />
+            
             <Image
               src="/google.png"
               height={30}
               width={30}
             />
             Continue with Google
+          </button>
+          <button onClick={()=>{
+            signIn("github",{callbackUrl:"https://localhost:3000/"})
+          }}>
+          <Image
+            src="/github.png"
+            height={40}
+            width={40}
+            alt="Github"
+          />Continue with Github
           </button>
         </div>
       </>
--- factos/app/layout.js
+++ factos/app/layout.js
@@ -7,6 +7,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <title>Facts Today</title>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
         <link href="https://fonts.googleapis.com/css2?family=Oxygen:wght@300;400;700&display=swap" rel="stylesheet"/>
