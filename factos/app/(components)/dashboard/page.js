1afebe5895ed34e381a4487601d206ef68bbc427
Ankit Khanal <akhanal749@gmail.com>
2024-03-14 16:43:53 +0545

    add

--- factos/app/(components)/dashboard/page.js
+++ factos/app/(components)/dashboard/page.js
@@ -1,14 +1,51 @@
+// "use client"
+// import { signOut } from "next-auth/react";
+// import { useSession } from "next-auth/react";
+// import Link from "next/link";
+// import Image from "next/image";
+// export default function Dashboard(){
+//     const handleClick = ()=>{
+//         if(confirm("Are you sure you want to logout?"))
+//         signOut({callbackUrl:"http://localhost:3000/facts"})
+//     }
+//     const {data: session} = useSession()
+//     return(
+//         <>
+//             <div className="dashboard-container">
+//                 <div className="go-back">
+//                     <Link href="/facts">
+//                         <h3 title="Facts">{"< "}Go Back</h3>
+//                     </Link>
+//                 </div>
+//                 <h1>Dashboard</h1>
+//                 <Image
+//                     src={session.user.image}
+//                     height = {100}
+//                     width  = {100}
+//                     alt = "photo"
+//                 />
+//                 <h2>{session.user.name}</h2>
+//                 <button onClick={handleClick}
+//                 >Logout</button>
+//             </div>
+//         </>
+//     );
+// }
+
 "use client"
 import { signOut } from "next-auth/react";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import Image from "next/image";
+
 export default function Dashboard(){
     const handleClick = ()=>{
         if(confirm("Are you sure you want to logout?"))
         signOut({callbackUrl:"http://localhost:3000/facts"})
     }
+
     const {data: session} = useSession()
+
     return(
         <>
             <div className="dashboard-container">
@@ -18,15 +55,18 @@ export default function Dashboard(){
                     </Link>
                 </div>
                 <h1>Dashboard</h1>
-                <Image
-                    src={session?.user?.image}
-                    height = {100}
-                    width  = {100}
-                    alt = "photo"
-                />
-                <h2>{session?.user?.name}</h2>
-                <button onClick={handleClick}
-                >Logout</button>
+                {session && session.user && (
+                    <>
+                        <Image
+                            src={session.user.image}
+                            height = {100}
+                            width  = {100}
+                            alt = "photo"
+                        />
+                        <h2>{session.user.name}</h2>
+                    </>
+                )}
+                <button onClick={handleClick}>Logout</button>
             </div>
         </>
     );
